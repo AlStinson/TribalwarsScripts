4a029083e37f54b525a8d4aa581047a05f8932c4
Javier Delgado Cruces <javidelgado1997@gmail.com>
2023-02-14 21:57:51 +0100

    Fix error when totalResourceAssistant.js is used more than once

--- src/totalResourcesFarmAssistant.js
+++ src/totalResourcesFarmAssistant.js
@@ -1,12 +1,16 @@
-const list = $("#plunder_list tr");
-$("<th>Total</th>").insertBefore(list.first().children().eq(6));
-list.each(i => {
+var plunder_tr_list;
+if (!plunder_tr_list) {
+  plunder_tr_list = $("#plunder_list tr");
+  $("<th>Total</th>").insertBefore(plunder_tr_list.first().children().eq(6));
+}
+plunder_tr_list.each(i => {
     if (i<2) return;
-    const rows = list.eq(i).children();
-    const resources = rows.eq(5).children()
-    const wood = parseInt(resources.eq(0).children(".res").html());
-    const clay = parseInt(resources.eq(1).children(".res").html());
-    const iron = parseInt(resources.eq(2).children(".res").html());
+    const row = plunder_tr_list.eq(i).children();
+    if (row.eq(6).children(".res").length != 0) return;
+    const resources = row.eq(5).children()
+    const wood = parseInt(resources.eq(0).children().eq(1).html());
+    const clay = parseInt(resources.eq(1).children().eq(1).html());
+    const iron = parseInt(resources.eq(2).children().eq(1).html());
     const total = wood + clay + iron;
-    $("<td class='nowrap'><span class='icon header ressources'></span><span class='res'>"+total+"</span></td>").insertAfter(rows.eq(5))
+    $("<td class='nowrap'><span class='icon header ressources'></span><span class='res'>"+total+"</span></td>").insertAfter(row.eq(5))
 })
